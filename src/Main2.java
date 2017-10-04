59556491b76969c1d755aa452c85397feadc0436
Liaomandy <106022038@live.asia.edu.tw>
2017-10-04 14:56:00 +0800

    first

--- src/Main2.java
+++ src/Main2.java
@@ -11,12 +11,12 @@ public class Main2 {
         c=in.nextInt();
 
         int Sum=(a+b+c);
-        int edv=(a+b+c)/3;
+        int ave=(a+b+c)/3;
 
 
 
         System.out.println("Sum:" +Sum);
-        System.out.println("edv:" +edv);
+        System.out.println("ave:" +ave);
 
 
     }
---
+++ src/Main3.java
@@ -0,0 +1,14 @@
+public class Main3 {
+    public static void main(String[]args){
+        java.util.Scanner in=new java.util.Scanner(System.in);
+
+        System.out.println("公里:");
+        int km=in.nextInt();
+        Double mi=km*1.6;
+
+        System.out.println("公里:" +km);
+        System.out.println("英里:" +mi);
+
+
+    }
+}
